61cd49785a19a9db45bab894eecd9bea9903adce
Amy Fogelman <abfnj1@gmail.com>
2019-08-26 23:40:08 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,6 +1,6 @@
-class CreateStudents < ActiveRecord::Migration[5.1]
- def change
-    create_table :products do |t|
+class CreateStudents < ActiveRecord::Migration
+  def change
+    create_table :students do |t|
       t.string :name
     end
   end
